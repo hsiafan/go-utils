367324e8866a31db9076ca895b5f35e63d71379e
dongliu <dongliu@live.cn>
2024-08-16 19:44:21 +0800

    add seq methods to multi map

--- collection/maps2/multi_map.go
+++ collection/maps2/multi_map.go
@@ -1,9 +1,25 @@
 package maps2
 
-import "github.com/hsiafan/go-utils/collection/pair"
+import (
+	"iter"
+
+	"github.com/hsiafan/go-utils/collection/pair"
+)
 
 // MultiMap can hold multi value of one key
 type MultiMap[K comparable, V any] map[K][]V
+
+func New[K comparable, V any]() MultiMap[K, V] {
+	return MultiMap[K, V]{}
+}
+
+func Collect[K comparable, V any](seq iter.Seq2[K, V]) MultiMap[K, V] {
+	m := MultiMap[K, V]{}
+	for k, v := range seq {
+		m[k] = append(m[k], v)
+	}
+	return m
+}
 
 // Get gets values for key
 func (m MultiMap[K, V]) Get(k K) []V {
@@ -18,6 +34,40 @@ func (m MultiMap[K, V]) Add(k K, v V) {
 // Remove removes k and all values
 func (m MultiMap[K, V]) Remove(k K) {
 	delete(m, k)
+}
+
+func (m MultiMap[K, V]) All() iter.Seq2[K, V] {
+	return func(yield func(K, V) bool) {
+		for k, vs := range m {
+			for _, v := range vs {
+				if !yield(k, v) {
+					return
+				}
+			}
+		}
+	}
+}
+
+func (m MultiMap[K, V]) Keys() iter.Seq[K] {
+	return func(yield func(K) bool) {
+		for k, _ := range m {
+			if !yield(k) {
+				break
+			}
+		}
+	}
+}
+
+func (m MultiMap[K, V]) Values() iter.Seq[V] {
+	return func(yield func(V) bool) {
+		for _, vs := range m {
+			for _, v := range vs {
+				if !yield(v) {
+					return
+				}
+			}
+		}
+	}
 }
 
 // Entries returns a Pair slice contains the key-value items in map.
--- collection/maps2/uniq_multi_map.go
+++ collection/maps2/uniq_multi_map.go
@@ -1,12 +1,26 @@
 package maps2
 
 import (
+	"iter"
+
 	"github.com/hsiafan/go-utils/collection/pair"
 	"github.com/hsiafan/go-utils/collection/set"
 )
 
 // UniqMultiMap can hold multi value of one key, the same value only store once
 type UniqMultiMap[K comparable, V comparable] map[K]set.Set[V]
+
+func NewUniq[K comparable, V comparable]() UniqMultiMap[K, V] {
+	return UniqMultiMap[K, V]{}
+}
+
+func CollectUniq[K comparable, V comparable](seq iter.Seq2[K, V]) UniqMultiMap[K, V] {
+	m := UniqMultiMap[K, V]{}
+	for k, v := range seq {
+		m.Add(k, v)
+	}
+	return m
+}
 
 // Get returns values for key
 func (m UniqMultiMap[K, V]) Get(k K) set.Set[V] {
@@ -25,6 +39,40 @@ func (m UniqMultiMap[K, V]) Add(k K, v V) {
 // Remove removes k and all values
 func (m UniqMultiMap[K, V]) Remove(k K) {
 	delete(m, k)
+}
+
+func (m UniqMultiMap[K, V]) All() iter.Seq2[K, V] {
+	return func(yield func(K, V) bool) {
+		for k, vs := range m {
+			for v := range vs {
+				if !yield(k, v) {
+					return
+				}
+			}
+		}
+	}
+}
+
+func (m UniqMultiMap[K, V]) Keys() iter.Seq[K] {
+	return func(yield func(K) bool) {
+		for k, _ := range m {
+			if !yield(k) {
+				break
+			}
+		}
+	}
+}
+
+func (m UniqMultiMap[K, V]) Values() iter.Seq[V] {
+	return func(yield func(V) bool) {
+		for _, vs := range m {
+			for v := range vs {
+				if !yield(v) {
+					return
+				}
+			}
+		}
+	}
 }
 
 // Entries returns a Pair slice contains the key-value items in map.
